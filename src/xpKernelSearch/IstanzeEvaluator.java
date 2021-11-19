8a60603533c75431046831b3838b3a7d7a142f84
LospiX <xvrprn94@gmail.com>
2021-11-19 22:27:54 +0100

    last one

--- src/xpKernelSearch/IstanzeEvaluator.java
+++ src/xpKernelSearch/IstanzeEvaluator.java
@@ -14,25 +14,27 @@ public class IstanzeEvaluator {
 
     }
     public static void main (String[] args) throws Exception {
-        //List<File> files = new Explorer(".\\Istanze\\mps\\Class1Mps").retrieveFiles();
-        List<File> files = new Explorer(".\\Istanze\\mps\\SCmps").retrieveFiles();
+        List<File> files = new Explorer(".\\Istanze\\mps\\Class5Mps").retrieveFiles();
+        //List<File> files = new Explorer(".\\Istanze\\mps\\SCmps").retrieveFiles();
         String pathlog = ".\\log";
         String pathConfig = ".\\config.txt";
         Configuration config = ConfigurationReader.read(pathConfig);
-        String resTxt = "";
+
         List<File> istanzeToSolve= new ArrayList<>();
         for (File f : files) {
-            if (!f.getName().startsWith("test5x500-SC(4)") && !f.getName().startsWith("test5x500-SC(5)"))
+            //if (!f.getName().startsWith("test5x500-SC(4)") && !f.getName().startsWith("test5x500-SC(5)"))
+            if (!f.getName().startsWith("prob5_005_100000_03"))
                 continue;
             istanzeToSolve.add(f);
             System.out.println(f.canRead());
         }
         String[] resLines = new String[istanzeToSolve.size()];
         for (File f: istanzeToSolve){
-            config.setKernelSize(1);
+
             resLines[istanzeToSolve.indexOf(f)] = f.getName().subSequence(0, f.getName().indexOf(".mps"))+" ;  ";
 
             //resLines[istanzeToSolve.indexOf(f)] = new MipResolver(f).solve()+"    "; // Solved by Gurobi
+            //config.setKernelSize(1);
             //resLines[istanzeToSolve.indexOf(f)] = new  KernelSearch(f.getPath(), pathlog, config).start().getObj()+"    "; // Solved by Gurobi
 
             config.setKernelSize(0.15);
@@ -42,7 +44,7 @@ public class IstanzeEvaluator {
             resLines[istanzeToSolve.indexOf(f)] += new KernelSearch(f.getPath(), pathlog, config).start().getObj()+" ;   "; // Solve by KerSerch with 10% dim buckets
             config.setBucketSize(0.25);
             resLines[istanzeToSolve.indexOf(f)] += new KernelSearch(f.getPath(), pathlog, config).start().getObj()+" ;   "; // Solve by KerSerch with 25% dim buckets
-            config.setBucketSize(0.4);
+            config.setBucketSize(0.33);
             resLines[istanzeToSolve.indexOf(f)] += new KernelSearch(f.getPath(), pathlog, config).start().getObj()+" ;   ";   // Solve by KerSerch with 40% dim bucketsconfig.setBucketSize(0.4);
             config.setBucketSize(0.5);
             resLines[istanzeToSolve.indexOf(f)] += new KernelSearch(f.getPath(), pathlog, config).start().getObj();   // Solve by KerSerch with 50% dim bucketsconfig.setBucketSize(0.4);
